ede2a7c5aaa5cc64c0f09a8b74a8ba32769a5daa
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2024-08-26 14:24:24 -0300

    fix date

--- backend/app/validators/campaign_validator.ts
+++ backend/app/validators/campaign_validator.ts
@@ -3,7 +3,11 @@ import { DateTime } from 'luxon';
 
 export const createCampaignValidator = vine.compile(
   vine.object({
+<<<<<<< HEAD
     date: vine.date().transform((value) => {
+=======
+    date: vine.date({ formats: { utc: true } }).transform((value) => {
+>>>>>>> parent of d7522fb (fix send recupera)
       return DateTime.fromJSDate(value);
     }),
     name: vine.string(),
