08be9728b709d98a8e209acec5addb6c63fad81f
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2024-08-29 19:05:13 -0300

    Change queue

---
+++ backend/commands/bulk_action_eme.ts
@@ -0,0 +1,24 @@
+import Action from '#models/action';
+import TypeAction from '#models/type_action';
+import { handleSendingForRecupera } from '#services/utils/recupera';
+import { BaseCommand } from '@adonisjs/core/ace';
+import type { CommandOptions } from '@adonisjs/core/types/ace';
+
+export default class BulkActionEme extends BaseCommand {
+  static commandName = 'bulk:action-eme';
+  static description = '';
+
+  static options: CommandOptions = {};
+
+  async run() {
+    const typeAction = await TypeAction.findBy('abbreviation', 'SMS');
+    if (typeAction) {
+      const actions = await Action.query().where('retornotexto', 'Em fila').where('typeActionId', typeAction.id).whereRaw('created_at::date = CURRENT_DATE');
+
+      for (const action of actions) {
+        await handleSendingForRecupera(action);
+      }
+
+    }
+  }
+}
---
+++ backend/commands/bulk_action_sms.ts
@@ -0,0 +1,26 @@
+import { BaseCommand } from '@adonisjs/core/ace';
+import type { CommandOptions } from '@adonisjs/core/types/ace';
+import Action from '#models/action';
+import TypeAction from '#models/type_action';
+import { handleSendingForRecupera } from '#services/utils/recupera';
+
+export default class BulkActionSms extends BaseCommand {
+  static commandName = 'bulk:action-sms';
+  static description = 'Anvio de acionamentos de sms retidos';
+
+  static options: CommandOptions = {
+    startApp: true,
+  };
+
+  async run() {
+    const typeAction = await TypeAction.findBy('abbreviation', 'SMS');
+    if (typeAction) {
+      const actions = await Action.query().where('retornotexto', 'Em fila').where('typeActionId', typeAction.id).whereRaw('created_at::date = CURRENT_DATE');
+
+      for (const action of actions) {
+        await handleSendingForRecupera(action);
+      }
+
+    }
+  }
+}
