348db31bf9afcfffa27db7a440441535201053c2
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2024-10-07 16:00:34 -0300

    feature actions and campaing

---
+++ frontend-next/ecosystem.config.cjs
@@ -0,0 +1,34 @@
+//const severPath = "/Users/thiago/Projects/Yuan/easycob/frontend/";
+const severPath = "/data/app/easycob/frontend-next/";
+
+require("dotenv").config({ path: `${severPath}.env` });
+
+module.exports = {
+  apps: [
+    {
+      name: 'NextAppEasycob',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      env_local: {
+        APP_ENV: 'local' // APP_ENV=local
+      },
+      env_dev: {
+        APP_ENV: 'dev' // APP_ENV=dev
+      },
+      env_prod: {
+        APP_ENV: 'prod' // APP_ENV=prod
+      }
+    }
+  ],
+};
+
+/*       env: {
+        TZ: "America/Fortaleza",
+        AUTH_SECRET:
+          "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+        API_BASE_URL: "http://192.168.1.194:4444",
+        NUXT_PUBLIC_API_BASE: "http://192.168.1.194:4444",
+        BASE_URL: "http://192.168.1.194:4000",
+      }, */
