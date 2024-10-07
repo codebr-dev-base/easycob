6194665c21dde3d803d0c436716972b9f2633370
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2024-10-07 16:53:03 -0300

    feature actions and campaing

--- frontend-next/app/layout.tsx
+++ frontend-next/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Ubuntu } from "next/font/google";
+import { Inter } from "next/font/google";
 import "@/app/assets/css/globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { CookiesProvider } from "next-client-cookies/server";
 import AuthProvider from "./providers/AuthContext";
 import { getAccessToken } from "./lib/auth";
 
-const inter = Ubuntu({
-  weight: ["300", "400", "500", "700"],
-  subsets: ["latin-ext"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Easycob",
