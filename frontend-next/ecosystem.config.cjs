b97ee3f0ea947cbe00627c2ba099e8963ce80cb2
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2024-10-07 16:16:45 -0300

    feature actions and campaing

--- frontend-next/ecosystem.config.cjs
+++ frontend-next/ecosystem.config.cjs
@@ -6,21 +6,12 @@ require("dotenv").config({ path: `${severPath}.env` });
 module.exports = {
   apps: [
     {
-      name: 'NextAppEasycob',
-      exec_mode: 'cluster',
-      instances: 'max', // Or a number of instances
-      script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 4000',
-      env_local: {
-        APP_ENV: 'local' // APP_ENV=local
-      },
-      env_dev: {
-        APP_ENV: 'dev' // APP_ENV=dev
-      },
-      env_prod: {
-        APP_ENV: 'prod' // APP_ENV=prod
-      }
-    }
+      name: "NextAppEasycob",
+      exec_mode: "cluster",
+      instances: "1", // Or a number of instances
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 4000",
+    },
   ],
 };
 
