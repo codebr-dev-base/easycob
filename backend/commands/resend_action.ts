5873e34708c3afe804d0e710174f9edc8d001433
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2025-01-16 16:21:48 -0300

    make commander send recupera

---
+++ backend/commands/resend_action.ts
@@ -0,0 +1,25 @@
+import { BaseCommand } from '@adonisjs/core/ace';
+import type { CommandOptions } from '@adonisjs/core/types/ace';
+import { handleSendingForRecupera } from '#services/utils/recupera';
+import Action from '#models/action';
+
+export default class ResendAction extends BaseCommand {
+  static commandName = 'resend:action';
+  static description = '';
+
+  static options: CommandOptions = {
+    startApp: true,
+  };
+
+  async run() {
+    this.logger.info('Hello world from "ResendAction"');
+
+    const actions = await Action.query()
+      .whereILike('retornotexto', 'Em fila')
+      .whereRaw('created_at::date = CURRENT_DATE');
+
+    for (const action of actions) {
+      await handleSendingForRecupera(action);
+    }
+  }
+}
