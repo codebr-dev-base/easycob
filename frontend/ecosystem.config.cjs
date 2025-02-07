d54259e440febb8f5c974be28bbde60c95dfd427
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2025-02-07 08:04:57 -0300

    incremente instances front

--- frontend/ecosystem.config.cjs
+++ frontend/ecosystem.config.cjs
@@ -14,7 +14,7 @@ module.exports = {
         NODE_ENV: "production", // Define a variável de ambiente NODE_ENV como 'production'
         PORT: 4000, // Define a porta em que a aplicação será executada
       },
-      instances: 1, // Número de instâncias, 1 para execução única
+      instances: 2, // Número de instâncias, 1 para execução única
       autorestart: true, // Habilita o reinício automático em caso de falha
     },
   ],
