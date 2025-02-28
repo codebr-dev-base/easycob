5a06dc797774318d00d3afe5242b6a71eebc8823
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2025-02-27 21:21:44 -0300

    fix

--- backend/commands/fix_send_lost_action_sms.ts
+++ backend/commands/fix_send_lost_action_sms.ts
@@ -19,33 +19,30 @@ export default class FixSendLostActionSms extends BaseCommand {
   async getPendingLots(type = 'SMS') {
     const result = await db.rawQuery(`
       WITH actions_filtered AS (
-        SELECT *
-        FROM public.actions
-        WHERE created_at::date > '2025-02-20'
-    ),
-    campaign_lots_filtered AS (
-        SELECT *
-        FROM public.campaign_lots
-        WHERE created_at::date > '2025-02-20'
+          SELECT *
+          FROM public.actions
+          WHERE created_at::date > '2025-02-20'
+      ),
+      campaign_lots_filtered AS (
+          SELECT *
+          FROM public.campaign_lots
+          WHERE created_at::date > '2025-02-20'
           AND messageid IS NOT NULL
-          AND codigo_status IS NULL
-          AND data_retorno IS NULL
-    )
-
-    SELECT
+          AND codigo_status is null
+          AND data_retorno is null
+      )
+      SELECT
         cl.id,
-        cl.cod_credor_des_regis,
         cl.contato,
-        cl.standardized
-    FROM campaign_lots_filtered cl
-    LEFT JOIN actions_filtered a
-        ON cl.standardized = a.contato
-        AND cl.cod_credor_des_regis = CAST(a.cod_credor_des_regis AS VARCHAR)
-        AND cl.created_at::date = a.created_at::date
-    JOIN campaigns c ON cl.campaign_id = c.id
-    WHERE a.id IS NULL
+        cl.cod_credor_des_regis,
+        cl.campaign_id
+      FROM campaign_lots_filtered cl
+      LEFT JOIN actions_filtered a
+          ON cl.contato = a.contato
+          AND cl.created_at::date = a.created_at::date
+      JOIN campaigns c ON cl.campaign_id = c.id
+      WHERE a.id IS NULL
       AND c.type = '${type}'
-    ORDER BY cl.id ASC;
   `);
     return result.rows;
   }
@@ -59,6 +56,7 @@ export default class FixSendLostActionSms extends BaseCommand {
       const clients = await getClients(lots);
       const clientsGroups = lodash.groupBy(clients, 'contato');
       for (const item of lots) {
+        this.logger.info(JSON.stringify(item));
         const campaign = await Campaign.find(item.campaign_id);
         if (!campaign) {
           this.logger.error('Not find campaign');
@@ -66,7 +64,6 @@ export default class FixSendLostActionSms extends BaseCommand {
         }
 
         await smsService.createAction(item, clientsGroups, campaign);
-        this.logger.info(JSON.stringify(item));
         const lot = await CampaignLot.find(item.id);
         if (!lot) {
           this.logger.error('Not find lot');
