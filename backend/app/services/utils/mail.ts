e28d852eecd3b1c8e430b417a1febd02fd6806cd
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2025-03-18 09:37:47 -0300

    Fix send invoice

--- backend/app/services/utils/mail.ts
+++ backend/app/services/utils/mail.ts
@@ -175,7 +175,7 @@ export const sendMailByApiSimple = async (
       }),
     });
 
-    console.log(response);
+    console.log(response.json());
     if (!response.ok) {
       throw new Error(
         `Erro na requisição: ${response.status} - ${response.statusText}`
