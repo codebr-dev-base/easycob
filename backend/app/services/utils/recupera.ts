e04b84af4ece7eb17b95759664a40f98ec3c0d33
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2025-03-26 17:44:19 -0300

    fix send recupera

--- backend/app/services/utils/recupera.ts
+++ backend/app/services/utils/recupera.ts
@@ -126,7 +126,7 @@ export async function dispatchToRecupera(
     wallet: action.wallet,
   };
 
-  console.log(item);
+  console.log(queueName);
 
   if (queueName) {
     await SendRecuperaJob.dispatch(item, {
