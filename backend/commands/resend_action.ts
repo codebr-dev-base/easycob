4f77795ca4a2da36c082f656a8b942e9b70da3cd
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2025-03-26 19:42:23 -0300

    fix send recupera

--- backend/commands/resend_action.ts
+++ backend/commands/resend_action.ts
@@ -16,9 +16,25 @@ export default class ResendAction extends BaseCommand {
   async run() {
     this.logger.info('Hello world from "ResendAction"');
 
+    const actionsSync = await Action.query()
+      .whereILike('retornotexto', 'Em fila')
+      .whereRaw(`created_at::date >= '2023-03-23'`)
+      .whereNotNull('result_sync');
+
+    for (const action of actionsSync) {
+      action.sync = true;
+      const resultSync = JSON.parse(action.resultSync);
+      action.syncedAt = DateTime.now();
+      action.retorno = <string>resultSync.XML?.RETORNO;
+      action.retornotexto = <string>resultSync.XML?.RETORNOTEXTO;
+      await action.save();
+      console.log(action);
+    }
+
     const actions = await Action.query()
       .whereILike('retornotexto', 'Em fila')
-      .whereRaw(`created_at::date >= '2023-03-23'`);
+      .whereRaw(`created_at::date >= '2023-03-23'`)
+      .whereNull('result_sync');
     for (const action of actions) {
       await handleSendingForRecupera(action);
       await new Promise((resolve) => setTimeout(resolve, 100));
