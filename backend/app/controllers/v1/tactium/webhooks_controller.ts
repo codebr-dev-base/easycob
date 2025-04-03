cb29d6db7bbe76200d4839dec877d5dd2c9550f3
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2025-04-03 14:46:08 -0300

    test login

--- backend/app/controllers/v1/tactium/webhooks_controller.ts
+++ backend/app/controllers/v1/tactium/webhooks_controller.ts
@@ -1,12 +1,39 @@
 import type { HttpContext } from '@adonisjs/core/http';
 import SocketIoProvider from '#providers/socket_io_provider';
 
+const eventoCodes = {
+  AGENTE_LOGON: 1,
+  AGENTE_LOGOFF: 2,
+  AGENTE_PAUSA: 3,
+  AGENTE_PRONTO: 4,
+  AGENTE_POS_ATENDIMENTO: 5,
+  LIGACAO_ENCERRADA: 10,
+  LIGACAO_COMPLETADA: 11,
+  LIGACAO_ATENDIDA: 12,
+  TRANSFERENCIA_REALIZADA: 13,
+  TRANSFERENCIA_COMPLETADA: 14,
+  TRANSFERENCIA_ATENDIDA: 15,
+  CONSULTA_ENCERRADA: 16,
+  CONSULTA_COMPLETADA: 17,
+  CONSULTA_ATENDIDA: 18,
+  GRAVACAO_INICIADA: 30,
+  GRAVACAO_ENCERRADA: 31,
+};
+
 export default class WebhooksController {
   public async handle({ request, response }: HttpContext) {
     const io = SocketIoProvider.getInstance();
     const body = request.body();
-    console.log(body);
-    io.emit('webhook', body);
+
+    if (body.evento) {
+      switch (body.evento) {
+        case eventoCodes.AGENTE_LOGON:
+          io.emit('message', body);
+          console.log(body);
+          break;
+      }
+    }
+    //io.emit('webhook', body);
     response.status(200).send('OK');
   }
 }
